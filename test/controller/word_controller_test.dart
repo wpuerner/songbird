0287bdb0dec4b73a0d6295ca4c53d3d2d30a4a2a
wpuerner <williampuerner@gmail.com>
2020-04-25 10:29:04 -0500

    word controller unit tests and word settings loader

--- test/controller/word_controller_test.dart
+++ test/controller/word_controller_test.dart
@@ -1,9 +1,7 @@
-import 'package:songassociation/controller/word_controller.dart';
-import 'package:global_configuration/global_configuration.dart';
-
-import 'package:songassociation/model/word.dart';
-
 import 'package:flutter_test/flutter_test.dart';
+import 'package:songassociation/controller/word_controller.dart';
+import 'package:songassociation/controller/word_settings_loader.dart';
+import 'package:songassociation/model/word.dart';
 
 const MOCK_WORD = "mockWord";
 const MOCK_SONG_NAME = "mockSongName";
@@ -12,35 +10,78 @@ const MOCK_ARTIST_NAME = "mockArtistName";
 void main() {
 
   setUp(() {
-    Word mockWord = Word();
+    dynamic mockWord = Word();
     mockWord.word = MOCK_WORD;
     mockWord.examples = List<Example>();
     mockWord.examples.add(Example());
     mockWord.examples[0].songName = MOCK_SONG_NAME;
     mockWord.examples[0].artistName = MOCK_ARTIST_NAME;
-    Map<String, List<Word>> mockWordSettings = {
-      "words": [mockWord]
-    };
-    GlobalConfiguration().loadFromMap(mockWordSettings);
+    List<Word> mockWordList = List<Word>();
+    mockWordList.add(mockWord);
+    WordSettingsLoader().globalWordList = mockWordList;
   });
 
-  group('WordController initialization tests', () {
-    test('Initialized word list should contain valid data', () {
-      WordController().initialize(1);
-      expect(WordController().words[0].examples[0].songName, equals(MOCK_SONG_NAME));
-      expect(WordController().words[0].examples[0].artistName, equals(MOCK_ARTIST_NAME));
+  group('Initialization tests', () {
+    test('Words are placed in unplayedWordsQueue', () {
+      WordController().wordCountPerRound = 2;
+      WordController().initialize();
+      expect(WordController().unplayedWordsQueue.length, equals(2));
+      expect(WordController().playedWordsQueue.length, equals(0));
+      expect(WordController().wordResultsQueue.length, equals(0));
+    });
+
+    test('Example song and artist are included in Word object', () {
+      WordController().wordCountPerRound = 1;
+      WordController().initialize();
+      Word result = WordController().unplayedWordsQueue.removeFirst();
+      expect(result.examples[0].songName, equals(MOCK_SONG_NAME));
+      expect(result.examples[0].artistName, equals(MOCK_ARTIST_NAME));
+    });
+  });
+
+  group('Queue tests', () {
+    test('Unplayed words queue is empty when all words are played', () {
+      WordController().wordCountPerRound = 3;
+      WordController().initialize();
+      while(WordController().isNextWordAvailable()) {
+        WordController().getNextWord();
+      }
+      expect(WordController().unplayedWordsQueue.length, equals(0));
+    });
+
+    test('Played words results queues are full when all words played', () {
+      WordController().wordCountPerRound = 3;
+      WordController().initialize();
+      while(WordController().isNextWordAvailable()) {
+        WordController().submitWordResult(true);
+        WordController().getNextWord();
+      }
+      expect(WordController().playedWordsQueue.length, equals(3));
+      expect(WordController().wordResultsQueue.length, equals(3));
+    });
+  });
+
+  group('Get next word tests', () {
+    test('Get next word returns the next word', () {
+      WordController().wordCountPerRound = 1;
+      WordController().initialize();
+      expect(WordController().getNextWord(), equals(MOCK_WORD));
     });
 
     test('Word list loops to start of configuration', () {
-      WordController().initialize(2);
-      expect(WordController().words[1].examples[0].songName, equals(MOCK_SONG_NAME));
+      WordController().wordCountPerRound = 2;
+      WordController().initialize();
+      WordController().getNextWord();  //cycle the word so we get the second one
+      expect(WordController().isNextWordAvailable(), equals(true));
+      expect(WordController().getNextWord(), equals(MOCK_WORD));
     });
-  });
 
-  test('WordController returns next word', () {
-    WordController().initialize(1);
-    final word = WordController().getNextWord();
-    expect(word.examples[0].songName, equals(MOCK_SONG_NAME));
+    test('Return next word not available when the words queue is empty', () {
+      WordController().wordCountPerRound = 1;
+      WordController().initialize();
+      WordController().getNextWord();
+      expect(WordController().isNextWordAvailable(), equals(false));
+    });
   });
 }
 
