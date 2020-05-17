c5abc3ebb544e798e35b9db9c06a7b9ead8a45a2
wpuerner <williampuerner@gmail.com>
2020-05-17 12:02:29 -0500

    add keys for end screen buttons

--- test/screens/end/end_test.dart
+++ test/screens/end/end_test.dart
@@ -1,11 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:songassociation/screens/end/end.dart';
-
 import 'package:songassociation/controller/word_controller.dart';
-import 'package:songassociation/model/word.dart';
-import 'package:songassociation/screens/end/word_result_box.dart';
-import '../../test_constants.dart';
 import 'package:songassociation/controller/word_settings_loader.dart';
+import 'package:songassociation/model/word.dart';
+import 'package:songassociation/screens/end/end.dart';
+import 'package:songassociation/screens/end/word_result_box.dart';
+
+import '../../test_constants.dart';
+
+import 'package:flutter/material.dart';
 
 void main() {
 
@@ -32,6 +34,12 @@ void main() {
 
     final resultBoxFinder = find.byType(WordResultBox);
     expect(resultBoxFinder, findsNWidgets(5));
+
+    final restartButtonFinder = find.byKey(Key("restartButton"));
+    expect(restartButtonFinder, findsOneWidget);
+
+    final homeButtonFinder = find.byKey(Key("homeButton"));
+    expect(homeButtonFinder, findsOneWidget);
   });
 
 }
