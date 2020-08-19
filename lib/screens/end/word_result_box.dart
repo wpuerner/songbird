90ab9a8fa387b8766bea02b8842f998a603d8610
wpuerner <williampuerner@gmail.com>
2020-08-18 21:10:07 -0500

    colors for word result box

--- lib/screens/end/word_result_box.dart
+++ lib/screens/end/word_result_box.dart
@@ -18,38 +18,46 @@ class WordResultBox extends StatelessWidget {
     return Material(
         child: Directionality(
             textDirection: TextDirection.ltr,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Icon(isGotIt ? Icons.check : Icons.clear)
-                    )
-                  ),
-                  Text(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+              child: Container(
+                color: this.isGotIt ? Colors.lightBlue : Colors.redAccent,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(width: 10),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Icon(isGotIt ? Icons.check : Icons.clear)
+                      )
+                    ),
+                    Text(
                       word.word,
                       textAlign: TextAlign.center,
                       style: TextStyle(
-                          fontSize: 20
+                        fontSize: 20
                       )
-                  ),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child:  IconButton(
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child:  IconButton(
                           onPressed: () {
                             Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) =>
-                                    SuggestionScreen(word: this.word))
+                              context,
+                              MaterialPageRoute(builder: (context) =>
+                                SuggestionScreen(word: this.word))
                             );
                           },
                           icon: Icon(Icons.add_box)
+                        )
                       )
                     )
-                  )
-                ]
+                  ]
+                )
+              )
+
             )
         )
     );
